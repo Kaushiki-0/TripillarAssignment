6f2174c0ea4c31d45082a15b41d37541e1561b22
Kaushiki <kumarikaushiki375@gmail.com>
2026-03-04 15:47:13 +0530

    Update Ques2.java

--- Module4/Ques2.java
+++ Module4/Ques2.java
@@ -1,1 +1,31 @@
-Ques2.java
+interface Payment{
+    void pay();
+}
+class CreditCardPayment implements Payment{
+    int Amount;
+    CreditCardPayment(int Amount){
+        this.Amount=Amount;
+    }
+    public void pay(){
+        System.out.println("Payment is sucessful of "+Amount+" via Credit card");
+    }
+}
+class UPIpayment implements Payment{
+int Amount;
+UPIpayment(int Amount){
+    this.Amount=Amount;
+}
+public void pay(){
+    System.out.println("Payment is sucessful of "+Amount+" via UPI");
+
+}
+}
+class Ques2{
+    public static void main(String[] args){
+        CreditCardPayment s=new CreditCardPayment(10000);
+        s.pay();
+        UPIpayment g=new UPIpayment(20000);
+        g.pay();
+
+    }
+}
