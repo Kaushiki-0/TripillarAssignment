257644a467299b8c179d4863a51b8a72d0ac9224
Kaushiki <kumarikaushiki375@gmail.com>
2026-03-04 15:47:48 +0530

    Update Ques4.java

--- Module4/Ques4.java
+++ Module4/Ques4.java
@@ -1,1 +1,9 @@
-Ques4.java
+class Ques4{
+    public static void main(String[] args){
+        String s = "JAVA";
+        StringBuilder sb= new StringBuilder(s);
+        sb.reverse();
+        System.out.println(s);
+        System.out.println("After reversal:"+sb);
+    }
+}
