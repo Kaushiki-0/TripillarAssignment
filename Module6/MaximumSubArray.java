4565ae355b53522309e31e75cbad3c5fd964de3c
Kaushiki <kumarikaushiki375@gmail.com>
2026-03-09 23:35:29 +0530

    Create MaximumSubArray.java

---
+++ Module6/MaximumSubArray.java
@@ -0,0 +1,13 @@
+class MaximumSubArray {
+public static void main(String[] args){
+    int a[]={1,2,3,-2,5};
+    int n=a.length;
+    int cur_sum=a[0];
+    int max_sum=a[0];
+    for(int i=1;i<n;i++){
+    cur_sum=Math.max(a[i],cur_sum+a[i]);
+    max_sum=Math.max(cur_sum,max_sum);
+    }
+    System.out.println(max_sum);
+}
+}
